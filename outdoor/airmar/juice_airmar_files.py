e2c33f0664a7e5ae071c35e2bb6a9dd15b87290a
Patrick O'Keeffe <pokeeffe@wsu.edu>
2015-07-09 12:54:41 -0800

    Revise Airmar 200WX output file column names/units
    - Following naming convention applied within indoor datalogger files:
        [instrument model]_[scalar abbreviation]
    - Use standard units abbreviations

--- outdoor/airmar/juice_airmar_files.py
+++ outdoor/airmar/juice_airmar_files.py
@@ -37,27 +37,16 @@ if not args.dest:
 
 
 #list of relevant measurements recorded
-columns = ['"Date and Time",'
-          '"Barometric Pressure",'
-          '"Air Temperature",'
-          '"Relative Humidity",'
-          '"Dew Point",'
-          '"Wind Direction (True N)",'
-          '"Wind Direction (Magnetic N)",'
-          '"Wind Speed"']
-
-#list of units for recorded measurements
-column_units = ['"YYYY-MM-DD hh:mm:ss.sss(GMT -8)",'
-                '"milibars",'
-                '"degC",'
-                '"percent",'
-                '"degC",'
-                '"degTrue",'
-                '"degMag",'
-                '"m/s"']
-
-header = ",".join(columns) + "\n"
-units = ",".join(column_units) + '\n'
+columns = [('TIMESTAMP',      'GMT-8'),
+           ('m200WX_P',       'mbar'),
+           ('m200WX_T',       'degC'),
+           ('m200WX_RH',      '%'),
+           ('m200WX_dew_T',   'degC'),
+           ('m200WX_WD_true', 'degEofTN'),
+           ('m200WX_WD_mag',  'degEofMN'),
+           ('m200WX_WS',      'm/s')]
+col_names = ','.join(['"%s"' % ea[0] for ea in columns])
+col_units = ','.join(['"%s"' % ea[1] for ea in columns])
 
 
 def squeeze_met_data(airmar_filepath, date):
@@ -74,8 +63,8 @@ def squeeze_met_data(airmar_filepath, date):
 
     with open(osp.join(args.dest, oname), mode='w') as outfile:
        with open(airmar_filepath, mode='r') as datafile:
-            outfile.write(header)
-            outfile.write(units)
+            outfile.write(col_names+'\n')
+            outfile.write(col_units+'\n')
             for line in datafile:
                 #each data protocol key is divided by a $ identifier
                 time, data = line.split('$')
@@ -85,23 +74,16 @@ def squeeze_met_data(airmar_filepath, date):
                     (key, bpi, bpiu, bp, bpbu, at, atu, wt, wtu,
                      rh, ah, dp, dpu, wdt, wdtu, wdm, wdmu, wsk,
                      wsku, wsm, wsmu) = data.split(',')
-                #returns 'NAN' if no measurement was recorded
-                    #converts barometric pressure (bars) to mbars
-                    if bp:
-                        bp = "{:.1f}".format( float(bp)*1000 )
-                    else:
-                        bp = '"NAN"'
+                    #convert barometric pressure (bars) to mbars
+                    bp = "{:.1f}".format( float(bp)*1000 ) if bp else '"NAN"'
                     if not at: at = '"NAN"'
                     if not rh: rh = '"NAN"'
                     if not dp: dp = '"NAN"'
                     if not wdt: wdt = '"NAN"'
                     if not wdm: wdm = '"NAN"'
                     if not wsm: wsm = '"NAN"'
-                    dataout = (timestamp +
-                            "," + bp + "," + at + "," + rh +
-                            "," + dp + "," + wdt + "," + wdm +
-                            "," + wsm +'\n')
-                    outfile.write(dataout)
+                    csvrec = ','.join([timestamp, bp, at, rh, dp, wdt, wdm, wsm])
+                    outfile.write(csvrec+'\n')
 
 
 rawfiles = glob(osp.join(args.source, '*.log'))
