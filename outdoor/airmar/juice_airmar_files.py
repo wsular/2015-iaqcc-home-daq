74892abe75fe35da62445bf1f0606a985bd84b3c
Patrick O'Keeffe <pokeeffe@wsu.edu>
2015-07-09 12:24:58 -0800

    ENH: search a folder and re-write all Airmar files
    - Source files remain intact
    - Destination directory defaults to working dir
    - Can specify both source and destination directories
      using command line parameters
    - Generated files retain input file name, except with
      different suffix: `.dat`

--- outdoor/airmar/juice_airmar_files.py
+++ outdoor/airmar/juice_airmar_files.py
@@ -1,29 +1,40 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Airmar data file juicer
 
-This is a temporary script file.
+Searches a directory for data files made by Airmar weather station and
+re-writes files into comma-separated format containing:
+
+* ISO8601-compliant timestamp
+* Barometric pressure, mbar
+* Ambient temperature, degC
+* Relative humidity, %
+* Dew point temperature, degC
+* Wind direction, degEofTN and degEofMN
+* Wind speed, m/s
+
+The search directory is provided as first command line argument; if not
+specified, it defaults to the working directory. Similarly, the output
+directory can be provided as the second command line argument and, if missing,
+will default to the working directory.
+
 """
 
-import os.path as osp
-#import sys
+import os, os.path as osp
+import argparse
 
-#import glob
-fpath = r'C:\Users\lar\Documents\Airmar WeatherCaster\WeatherCaster 3_005\Raw Data Log'
-#files = glob.glob(fpath+'\*.log')
-#for ea in files:
-#    print ea
+from datetime import datetime
+from glob import glob
 
-#if sys.argv[1]:
-#    fname = sys.argv[1].strip('"')
-#else:
-fname = r'Jul_07_2015_3201670_0183.LOG'
+opts = argparse.ArgumentParser()
+opts.add_argument("source",
+                  help="Full path to directory containing input files")
+opts.add_argument("-d", "--dest",
+                  help="Full path to output directory; default: working directory")
+args = opts.parse_args()
+if not args.dest:
+    args.dest = os.getcwd()
 
-#Date for the current file being processed. Be sure this accurately
-#reflects the data. To be used with 'inputdate' variable.
-YYYY_MM_DD = "2015-07-07" 
-
-oname = "fixed_"+fname
 
 #list of relevant measurements recorded
 columns = ['"Date and Time",'
@@ -35,7 +46,7 @@ columns = ['"Date and Time",'
           '"Wind Direction (Magnetic N)",'
           '"Wind Speed"']
 
-#list of units for recorded measurements          
+#list of units for recorded measurements
 column_units = ['"YYYY-MM-DD hh:mm:ss.sss(GMT -8)",'
                 '"milibars",'
                 '"degC",'
@@ -43,46 +54,67 @@ column_units = ['"YYYY-MM-DD hh:mm:ss.sss(GMT -8)",'
                 '"degC",'
                 '"degTrue",'
                 '"degMag",'
-                '"m/s"']         
-          
+                '"m/s"']
+
 header = ",".join(columns) + "\n"
 units = ",".join(column_units) + '\n'
 
 
-with open(osp.join(fpath, oname), mode='w') as outfile:
-   with open(osp.join(fpath, fname)) as datafile:              
-        outfile.write(header)
-        outfile.write(units)
-        for line in datafile:
-            #each data protocol key is divided by a $ identifier
-            time, data = line.split('$')
-            #autostamp rips date from file name
-            autostamp = '"' + fname[:-17] + " " + time[:-5] + '"'
-            #inputstamp uses date entered at top of code           
-            inputstamp = '"' + YYYY_MM_DD + " " + time[:-5] + '"'
-            #WIMDA is data protocol key for the meteorological composite
-            if data.startswith('WIMDA'):
-                (key, bpi, bpiu, bp, bpbu, at, atu, wt, wtu, 
-                 rh, ah, dp, dpu, wdt, wdtu, wdm, wdmu, wsk, 
-                 wsku, wsm, wsmu) = data.split(',')
-            #returns 'NAN' if no measurement was recorded
-                #converts barometric pressure (bars) to mbars
-                if bp:
-                    bp = "{:.1f}".format( float(bp)*1000 )
-                else: 
-                    bp = '"NAN"'
-                if not at: at = '"NAN"'
-                if not rh: rh = '"NAN"'
-                if not dp: dp = '"NAN"'
-                if not wdt: wdt = '"NAN"'
-                if not wdm: wdm = '"NAN"'
-                if not wsm: wsm = '"NAN"'                
-                dataout = (inputstamp + 
-                        "," + bp + "," + at + "," + rh + 
-                        "," + dp + "," + wdt + "," + wdm + 
-                        "," + wsm +'\n')
-                outfile.write(dataout)
-            
+def squeeze_met_data(airmar_filepath, date):
+    """Extract meterological data from raw 200WX data file"""
+    fpath, fname = osp.split(airmar_filepath)
+    oname = osp.splitext(fname)[0] + '.dat'
+
+    print "Extracting data from", fname
+    try:
+        os.makedirs(args.dest)
+    except OSError:
+        if not osp.isdir(args.dest):
+            raise
+
+    with open(osp.join(args.dest, oname), mode='w') as outfile:
+       with open(airmar_filepath, mode='r') as datafile:
+            outfile.write(header)
+            outfile.write(units)
+            for line in datafile:
+                #each data protocol key is divided by a $ identifier
+                time, data = line.split('$')
+                timestamp = date.strftime('"%Y-%m-%d ' + time[:-5] + '"' )
+                #WIMDA is data protocol key for the meteorological composite
+                if data.startswith('WIMDA'):
+                    (key, bpi, bpiu, bp, bpbu, at, atu, wt, wtu,
+                     rh, ah, dp, dpu, wdt, wdtu, wdm, wdmu, wsk,
+                     wsku, wsm, wsmu) = data.split(',')
+                #returns 'NAN' if no measurement was recorded
+                    #converts barometric pressure (bars) to mbars
+                    if bp:
+                        bp = "{:.1f}".format( float(bp)*1000 )
+                    else:
+                        bp = '"NAN"'
+                    if not at: at = '"NAN"'
+                    if not rh: rh = '"NAN"'
+                    if not dp: dp = '"NAN"'
+                    if not wdt: wdt = '"NAN"'
+                    if not wdm: wdm = '"NAN"'
+                    if not wsm: wsm = '"NAN"'
+                    dataout = (timestamp +
+                            "," + bp + "," + at + "," + rh +
+                            "," + dp + "," + wdt + "," + wdm +
+                            "," + wsm +'\n')
+                    outfile.write(dataout)
+
+
+rawfiles = glob(osp.join(args.source, '*.log'))
+for fpath in rawfiles:
+    try:
+        fname = osp.basename(fpath)
+        date = datetime.strptime(fname[:11], "%b_%d_%Y")
+        squeeze_met_data(fpath, date)
+    except (IndexError, ValueError):
+        print "Skipping unrecognizable file:", fname
+
+
+
 
 
 
