94b55520cfd80fc2e3805997ce35f33ab0e54993
Patrick O'Keeffe <pokeeffe@wsu.edu>
2016-02-27 19:53:52 -0800

    Update weather station (new RPi-based DAQ)

--- source/weather/backup_WX_data.bat
+++
@@ -1,4 +0,0 @@
-
-XCOPY /S /V /C /I /Y /F /D "C:\Users\lar\Desktop\200WX csv format\*.*" "E:\IAQ data\200WX csv\"
-XCOPY /S /V /C /I /Y /F /D "C:\Users\lar\Desktop\200WX raw data\*.*" "E:\IAQ data\200WX nmea\"
-TIMEOUT 10
